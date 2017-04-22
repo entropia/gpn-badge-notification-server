b8d0cd2900fe14a2f7c10eb1fd7e5bf1a3b976a5
Anton Schirg <anton.schirg@posteo.de>
2017-04-22 02:25:35 +0200

    simple web form

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-from flask import Flask, jsonify
+import flask
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 import json
 import time
 import psycopg2
 from psycopg2.extras import RealDictCursor
-
-db = "dbname=gulasch_notifier"
-conn = psycopg2.connect(db)
-
-cur = conn.cursor(cursor_factory=RealDictCursor)
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(flask.g, 'db_conn'):
+        db = "dbname=gulasch_notifier"
+        conn = psycopg2.connect(db)
+        flask.g.db_conn = conn
+    return flask.g.db_conn
+
+def get_cursor():
+    db = get_db()
+    return db.cursor(cursor_factory=RealDictCursor)
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(flask.g, 'db_conn'):
+        flask.g.db_conn.close()
+
 class Notification(json.JSONEncoder):
-    def __init__(self, id, valid_from, valid_to, summary, description, location):
+    def __init__(self, id=None, valid_from=None, valid_to=None, summary=None, description=None, location=None):
         self.id = id
         self.valid_from = valid_from
         self.valid_to = valid_to
@@ -31,14 +48,29 @@ class Notification(json.JSONEncoder):
                 }
 
     def insert(self):
-        cur.execute("INSERT INTO notifications (summary, description, location, valid_from, valid_to) VALUES (%s, %s, %s, %s, %s) RETURNING id", self.summary, self.description, self.location, self.valid_from, self.valid_to)
-        self.id = cur.fetchone()['id']
+        with get_cursor() as cur:
+            cur.execute("INSERT INTO notifications (summary, description, location, valid_from, valid_to) VALUES (%s, %s, %s, %s, %s) RETURNING id", (self.summary, self.description, self.location, self.valid_from, self.valid_to))
+            self.id = cur.fetchone()['id']
+        get_db().commit()
+
+    def delete(self):
+        with get_cursor() as cur:
+            cur.execute("DELETE FROM notifications WHERE id = %s", (self.id, ))
+        get_db().commit()
 
     @classmethod
     def get_active_notifications(self):
-        current_time = int(time.time() * 1000)
-        cur.execute("SELECT id, summary, description, location, valid_from, valid_to FROM notifications WHERE valid_from < %s AND valid_to > %s", (current_time + 30000, current_time))
-        return map(lambda d: Notification(**d), cur.fetchall())
+        with get_cursor() as cur:
+            current_time = datetime.now()
+            send_earlier = timedelta(seconds=30)
+            cur.execute("SELECT id, summary, description, location, valid_from, valid_to FROM notifications WHERE valid_from < %s AND valid_to > %s", (current_time + send_earlier, current_time))
+            return map(lambda d: Notification(**d), cur.fetchall())
+
+    @classmethod
+    def get_notifications(self):
+        with get_cursor() as cur:
+            cur.execute("SELECT id, summary, description, location, valid_from, valid_to FROM notifications")
+            return map(lambda d: Notification(**d), cur.fetchall())
         
 
 @app.route("/")
@@ -51,6 +83,27 @@ def poll():
                 'server_time': int(time.time() * 1000),
                 'notifications': map(Notification.to_dict, Notification.get_active_notifications())
             })
+
+date_fmt = '%c'
+
+@app.route("/manage")
+def list_notifications():
+    notifications = Notification.get_notifications()
+    now = datetime.now()
+    return render_template('notifications_list.html', notifications=notifications, current_time=now.strftime(date_fmt))
+
+@app.route("/manage/add", methods=['POST'])
+def new_notification():
+    valid_from = datetime.strptime(request.form['valid_from'], date_fmt)
+    valid_to = datetime.strptime(request.form['valid_to'], date_fmt)
+    new_notification = Notification(summary=request.form['summary'], description=request.form['description'], valid_from=valid_from, valid_to=valid_to)
+    new_notification.insert()
+    return redirect(url_for('list_notifications'))
+
+@app.route("/manage/delete", methods=['POST'])
+def delete_notification():
+    Notification(id=int(request.form['id'])).delete()
+    return redirect(url_for('list_notifications'))
 
 if __name__ == "__main__":
     app.run(debug=True)
