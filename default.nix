cc7cce7587b6ef25affd747f5cb999d7d49bc015
Anton Schirg <anton.schirg@posteo.de>
2017-05-01 13:43:44 +0200

    nix-shell

---
+++ default.nix
@@ -0,0 +1,10 @@
+let
+  pkgs = import <nixpkgs> {};
+  stdenv = pkgs.stdenv;
+  pypkgs = pkgs.python36Packages;
+in stdenv.mkDerivation rec {
+  version = "0.0.1";
+  name = "gpn-badge-notification-server-${version}";
+  src = ./.;
+  buildInputs = [pypkgs.flask pypkgs.psycopg2];
+}
